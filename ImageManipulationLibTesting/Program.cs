507a239b972eb1fb2a2710afae1180cda092a311
Yogev Strauber <yogev@drushim.co.il>
2023-05-01 14:36:32 +0300

    Add project files.

---
+++ ImageManipulationLibTesting/Program.cs
@@ -0,0 +1,12 @@
+﻿using ImageManipulationLibTesting;
+
+Random random = new Random();
+int x = random.Next();
+Image image = Image.Load(@"C:\Users\yogev.DRUSHIM\Desktop\outputs\sample.jpg");
+int desiredHeight = 50;
+int desiredWidth = 50;
+
+Image resizedImage = ImageUtils.ResizeImage(image, desiredHeight, desiredWidth);
+resizedImage.Save(@$"C:\Users\yogev.DRUSHIM\Desktop\outputs\output_{x}.jpg");
+
+
